55ff235cd747ef8561b31de637dc7b7b8c1d9837
meyara92 <melanie.arakaki92@gmail.com>
2020-08-20 19:02:18 -0300

    All changes

---
+++ ArakakiMelanie_Comision2_PuraCepa/js/lottie-custom-subscription-1.js
@@ -0,0 +1,20 @@
+//Caja 2
+const lottie_2_container = document.getElementById("suscription-2");
+const lottie2 = document.getElementById("lottie2")
+lottie_2_container.addEventListener('mouseenter', () =>{
+    lottie2.play();
+})
+lottie_2_container.addEventListener("mouseleave", ()=>{
+    lottie2.stop();
+})
+
+//Caja 3
+const lottie_3_container = document.getElementById("suscription-3");
+const lottie3 = document.getElementById("lottie3")
+lottie_3_container.addEventListener('mouseenter', () =>{
+    lottie3.play();
+})
+lottie_3_container.addEventListener("mouseleave", ()=>{
+    lottie3.stop();
+})
+
---
+++ ArakakiMelanie_Comision2_PuraCepa/js/lottie-custom.js
@@ -0,0 +1,30 @@
+//Caja 1
+const lottie_1_container = document.getElementById("suscription-1");
+const lottie1 = document.getElementById("lottie1")
+lottie_1_container.addEventListener('mouseenter', () =>{
+    lottie1.play();
+})
+lottie_1_container.addEventListener("mouseleave", ()=>{
+    lottie1.stop();
+})
+
+//Caja 2
+const lottie_2_container = document.getElementById("suscription-2");
+const lottie2 = document.getElementById("lottie2")
+lottie_2_container.addEventListener('mouseenter', () =>{
+    lottie2.play();
+})
+lottie_2_container.addEventListener("mouseleave", ()=>{
+    lottie2.stop();
+})
+
+//Caja 3
+const lottie_3_container = document.getElementById("suscription-3");
+const lottie3 = document.getElementById("lottie3")
+lottie_3_container.addEventListener('mouseenter', () =>{
+    lottie3.play();
+})
+lottie_3_container.addEventListener("mouseleave", ()=>{
+    lottie3.stop();
+})
+
